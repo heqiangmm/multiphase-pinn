3bba4e531f83dbdc57a14d40e8f970976a886a9f
Niccolò Tonicello <niccolo.tonicello@gmail.com>
2024-01-18 09:36:10 +0100

    First 2D case of a rotating bubble (full period)

---
+++ RotatingBubble.py
@@ -0,0 +1,90 @@
+import torch
+
+from pina import Trainer
+from pina.solvers import PINN
+from pina.plotter import Plotter
+from pina.model import FeedForward
+from multiphase_problems import RotatingBubble, RotatingBubbleMass
+from utils import create_parser
+from pytorch_lightning.callbacks import Callback
+
+# ===================================================== #
+#                                                       #
+#  This script implements the two dimensional linear    #
+#  advection droplet problem. The Multiphase class is   #
+#  from TimeDependentProblem, SpatialProblem and we     #
+#  denote:                                              #
+#           u --> field variable                        #
+#           x --> spatial variable                      #
+#           y --> spatial variable                      #
+#           t --> temporal variable                     #
+#                                                       #
+# ===================================================== #
+    
+# Hard Network definition
+class HardNet(torch.nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.model = FeedForward(*args, **kwargs)
+
+    def forward(self, x):
+        force = (x['x']+0.5) * (x['y']+0.5) * (0.5-x['x']) * (0.5-x['y'])
+        return self.model(x)*force
+    
+# Allen Cahn coefficient update
+class UpdateCoeff_AllenCahn(Callback):
+    def on_train_epoch_end(self, trainer, __):
+        trainer.solver.problem.__class__.scale = min(1.0, trainer.current_epoch*1e-4)
+        return
+
+if __name__ == "__main__":
+
+    # get parser
+    args = create_parser()
+
+    # extra features
+    callback = [UpdateCoeff_AllenCahn()] if args.allen_cahn else []
+    
+    # define problem + discretize
+    problem = RotatingBubbleMass() if args.mass else RotatingBubble()
+    nx = 20
+    nt = 600
+    problem.discretise_domain(nx, 'grid', locations=['t0'])
+    problem.discretise_domain(nx, 'grid', locations=['D'], variables=['x','y'])
+    problem.discretise_domain(nt, 'grid', locations=['D'], variables=['t'])
+    if args.mass:
+        problem.discretise_domain(nx, 'grid'  , locations=['mass'], variables=['x','y'])
+        problem.discretise_domain(nt, 'grid'  , locations=['mass'], variables=['t'])
+
+    # make model
+    model = HardNet(input_dimensions=3, output_dimensions=1, func=torch.nn.Tanh, inner_size=20, n_layers=4)
+    if args.load_model: # loading if not training
+        model.load_state_dict(torch.load(args.load_model+'.pth'))
+
+    # make solver
+    solver = PINN(
+                problem=problem,
+                model=model,
+                optimizer=torch.optim.AdamW,
+                optimizer_kwargs={'lr' : args.lr})
+
+    # make the trainer
+    trainer = Trainer(solver=solver, callbacks=callback, max_epochs=args.epochs)
+
+    # train or load
+    if args.save_model:
+        # train model and save
+        trainer.train()
+        torch.save(model.state_dict(), args.save_model+'.pth')
+
+    # plot if needes
+    if args.plot:
+        pl = Plotter()
+        pl.plot(solver=solver)
+
+#        T = 2.0*torch.pi
+#        times = torch.linspace(0, T, 100)
+#        for i, t in enumerate(times):
+#            print(t)
+#            filename = "phi_" + str(i) + ".pdf"
+#            pl.plot(solver=solver, components=['phi'], fixed_variables={'t':t}, filename=filename)
--- multiphase_problems.py
+++ multiphase_problems.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 from pina.problem import TimeDependentProblem, SpatialProblem
 from pina.operators import grad, laplacian, div
@@ -69,12 +70,12 @@ class AdvectionInterface(TimeDependentProblem, SpatialProblem):
 class AdvectionInterfaceMass(AdvectionInterface):
 
     def mass_conservation(input_, output_):
-            mask = input_['t']==0
-            phi_expected = AdvectionInterface.phi_initial(input_).tensor[mask]
-            tot_mass = phi_expected.sum()
-            out_mass = [output_[(input_['t'] == t).flatten()].sum() for t in torch.unique(input_['t'])]
-            out_mass = torch.stack(out_mass)
-            return tot_mass - out_mass
+        mask = input_['t']==0
+        phi_expected = AdvectionInterface.phi_initial(input_).tensor[mask]
+        tot_mass = phi_expected.mean()
+        out_mass = [output_[(input_['t'] == t).flatten()].mean() for t in torch.unique(input_['t'])]
+        out_mass = torch.stack(out_mass)
+        return tot_mass - out_mass
 
     # problem condition statement
     conditions = {}
@@ -82,3 +83,79 @@ class AdvectionInterfaceMass(AdvectionInterface):
          conditions[key] = condition
 
     conditions['mass'] = Condition(location=CartesianDomain({'x': [0, 1], 't': [0., AdvectionInterface.T]}), equation= Equation(mass_conservation))
+
+class RotatingBubble(TimeDependentProblem, SpatialProblem):
+
+    # time
+    T = 2.0*torch.pi
+    scale = 0.00
+    eps = 0.005
+
+    # assign output/ spatial and temporal variables
+    output_variables = ['phi']
+    spatial_domain = CartesianDomain({'x': [-0.5, 0.5], 'y': [-0.5, 0.5]})
+    temporal_domain = CartesianDomain({'t': [0, T]})
+    center = LabelTensor(torch.tensor([[0.0, 0.0]]), labels=['x', 'y'])
+    
+    def phi_initial(input_):
+        eps = RotatingBubble.eps
+        x = input_.extract(['x'])
+        y = input_.extract(['y']) - 0.25
+        norm_ = torch.sqrt(x**2 + y**2)
+        phi_expected = 0.5*(1. + torch.tanh((norm_ - 0.15) / (2.*eps)))
+        return -phi_expected + 1.
+
+    # define initial condition
+    def initial_condition(input_, output_):
+        phi_expected = RotatingBubble.phi_initial(input_)
+        return output_.extract(['phi']) - phi_expected
+
+    def advection(input_, output_):
+        gradient  = grad(output_, input_)
+        dphi_t    = gradient.extract(['dphidt'])
+        dphi_x    = gradient.extract(['dphidx'])
+        dphi_y    = gradient.extract(['dphidy'])
+        x = input_.extract(['x'])
+        y = input_.extract(['y'])
+        t = input_.extract(['t'])
+        eps = RotatingBubble.eps
+        # compute residuals
+        u1    = - y
+        u2    =   x
+        u_max = np.sqrt(2.0*0.5**2)
+        normals1 = dphi_x/(torch.sqrt(dphi_x.pow(2)+dphi_y.pow(2)+1e-16)+1e-10)
+        normals2 = dphi_y/(torch.sqrt(dphi_x.pow(2)+dphi_y.pow(2)+1e-16)+1e-10)
+        normals1 = normals1.detach()
+        normals1 = normals2.detach()
+        sharp1 = (-output_*(1.0-output_)*normals1 + eps*dphi_x)*u_max
+        sharp2 = (-output_*(1.0-output_)*normals2 + eps*dphi_y)*u_max
+        sharp1 = LabelTensor(sharp1, labels=dphi_t.labels)
+        sharp2 = LabelTensor(sharp2, labels=dphi_t.labels)
+        sharp1 = grad(sharp1, input_, d=['x'])
+        sharp2 = grad(sharp2, input_, d=['y'])
+        sharp1 = LabelTensor(sharp1, labels=dphi_t.labels)
+        sharp2 = LabelTensor(sharp2, labels=dphi_t.labels)
+        return dphi_t + u1*dphi_x + u2*dphi_y - RotatingBubble.scale*(sharp1+sharp2)
+
+    # problem condition statement
+    conditions = {
+        't0': Condition(location=CartesianDomain({'x': [-0.5, 0.5], 'y': [-0.5, 0.5], 't': 0}), equation=Equation(initial_condition)),
+        'D': Condition(location=CartesianDomain({'x': [-0.5, 0.5], 'y': [-0.5, 0.5], 't': [0, T]}), equation=Equation(advection)),
+    }
+
+class RotatingBubbleMass(RotatingBubble):
+
+    def mass_conservation(input_, output_):
+        mask = input_['t']==0
+        phi_expected = RotatingBubble.phi_initial(input_).tensor[mask]
+        tot_mass = phi_expected.mean()
+        out_mass = [output_[(input_['t'] == t).flatten()].mean() for t in torch.unique(input_['t'])]
+        out_mass = torch.stack(out_mass)
+        return tot_mass - out_mass
+
+    # problem condition statement
+    conditions = {}
+    for key, condition in RotatingBubble.conditions.items():
+         conditions[key] = condition
+
+    conditions['mass'] = Condition(location=CartesianDomain({'x': [-0.5, 0.5], 'y': [-0.5, 0.5], 't': [0, RotatingBubble.T]}), equation= Equation(mass_conservation))
